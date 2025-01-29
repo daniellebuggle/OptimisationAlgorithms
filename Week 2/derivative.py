f524f3a24904e678b2e85a6dad607b02e45f94b9
Danielle <daniellebuggle7@gmail.com>
2025-01-29 17:27:12 +0000

    added function to plot comparison plot of derivate evaluations vs finite difference

--- Week 2/derivative.py
+++ Week 2/derivative.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import sympy
 import numpy as np
 
@@ -41,7 +42,6 @@ x_values = np.arange(1, 101)
 calculations = evaluate_derivative(x, f, x_values)
 print(calculations)
 
-
 delta = 0.01
 finite_derivatives = finite_difference(f, x, delta, x_values)
 
@@ -51,4 +51,27 @@ for x_val, d_val in zip(x_values, finite_derivatives):
     print(f"{x_val:>7.2f} | {d_val:>9.3f}")
 print(finite_difference(f, x, delta, x_values))
 
+
+def plot_comparison(x, y1, y2, xlim_range, ylim_range, y1_label, y2_label, xaxis_label, yaxis_label, title, file_name):
+    plt.plot(x, y1, label=y1_label, color='b')
+    plt.plot(x, y2, label=y2_label, color='r')
+
+    if xlim_range or ylim_range:
+        plt.xlim(xlim_range)
+        plt.ylim(ylim_range)
+
+    plt.xlabel(xaxis_label)
+    plt.ylabel(yaxis_label)
+    plt.title(title)
+    plt.savefig(file_name)
+
+
+plot_comparison(x_values, calculations, finite_derivatives, None, None, 'Derivative Evaluations',
+                'Finite Difference', 'x', 'y',
+                'Comparing Derivative Evaluation to Finite Difference', 'Normal.png')
+
+
+plot_comparison(x_values, np.log(calculations), np.log(finite_derivatives), [1, 2], [1,4], 'Derivative Evaluations',
+                'Finite Difference', 'x', 'log(y)',
+                'Logarithmic Scale: Comparing Derivative Evaluation to Finite Difference', 'log.png')
 
